be3afff30f6b8a328d992d497609f17c09a5ca8b
Kaua Menezes <kauamenezes6@gmail.com>
2026-04-03 20:40:22 -0300

    created the structure that every move should follow

---
+++ PokemonBattle.Api/Models/Move.cs
@@ -0,0 +1,23 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using PokemonBattle.Api.Enums;
+
+namespace PokemonBattle.Api.Models;
+
+public class Move
+{
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public PokemonTypes Type { get; set; }
+
+    [Required]
+    public int Power { get; set; }
+    
+    [Required]
+    public int Accuracy { get; set; }
+}
