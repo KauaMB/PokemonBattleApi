ea317ab1d0f60dd9abf3b02725a8884ce983ef9a
Kaua Menezes <kauamenezes6@gmail.com>
2026-04-15 16:01:38 -0300

    Finalizing the project with Docker

--- PokemonBattle.Api/Program.cs
+++ PokemonBattle.Api/Program.cs
@@ -35,9 +35,21 @@ if (app.Environment.IsDevelopment())
 }
 app.MapControllers();
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
 
 app.Run();
 
